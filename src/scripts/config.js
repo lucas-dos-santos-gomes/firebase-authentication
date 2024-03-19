e966b67ecbd687427a1af0c2935f809b6195cf56
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-03-19 15:45:18 -0300

    First commit - Base code config

---
+++ src/scripts/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC03XhKChQ6x-3LMOAPBoRTnSFoyBFle_Y",
+  authDomain: "etec-20240229.firebaseapp.com",
+  databaseURL: "https://etec-20240229-default-rtdb.firebaseio.com",
+  projectId: "etec-20240229",
+  storageBucket: "etec-20240229.appspot.com",
+  messagingSenderId: "756171881252",
+  appId: "1:756171881252:web:c80af0d20f1bcfb4c711b1",
+  measurementId: "G-6BXSJMZVS1"
+};
+
+// firebase.initializeApp(firebaseConfig);
+// let auth = firebase.auth();
